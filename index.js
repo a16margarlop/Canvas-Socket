94de8aa7869a2cb86f8049baf1aec1ebb14fa456
= <=>
2023-11-23 12:28:08 +0100

    all perfect

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-
-let users = []; // Almacenar la información de cada usuario
+let users = [];
+let drawings = [];
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -16,40 +16,36 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  socket.on('setUsername', ({ username, userId }) => {
-    // Asignar nombre y ID al socket
+  socket.on('setUsername', ({ username, userId, color }) => {
     socket.username = username;
     socket.userId = userId;
-
-    // Agregar usuario a la lista
-    users.push({ username, userId, x: 0, y: 0 }); // Inicialmente en la posición (0, 0)
-
-    // Emitir la lista actualizada de usuarios a todos los clientes
+    users.push({ username, userId, x: 0, y: 0, path: [], color });
     io.emit('mousemove', users);
+
+    socket.emit('drawing', drawings); // Enviar los dibujos existentes al nuevo usuario
 
     io.emit('userConnected', `${username} connected`);
   });
 
   socket.on('mousemove', ({ x, y, username, userId }) => {
-    // Actualizar la posición del usuario en la lista
     const userIndex = users.findIndex((user) => user.userId === userId);
     if (userIndex !== -1) {
-      users[userIndex].x = x;
-      users[userIndex].y = y;
+        users[userIndex].x = x;
+        users[userIndex].y = y;
     }
 
-    // Emitir la lista actualizada de usuarios a todos los clientes
     io.emit('mousemove', users);
+});
+
+  socket.on('drawing', (data) => {
+    drawings.push(data);
+    io.emit('drawing', drawings);
   });
 
   socket.on('disconnect', () => {
     if (socket.username) {
-      // Eliminar usuario de la lista al desconectarse
       users = users.filter((user) => user.userId !== socket.userId);
-
-      // Emitir la lista actualizada de usuarios a todos los clientes
       io.emit('mousemove', users);
-
       io.emit('userDisconnected', `${socket.username} disconnected`);
     }
   });
